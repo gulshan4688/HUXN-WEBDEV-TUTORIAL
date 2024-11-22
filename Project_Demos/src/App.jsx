687c968aa91a489eb451219576a98b7db4213444
Gulshan kumar <gulshankumar4688@gmail.com>
2024-11-22 01:16:03 +0100

    added new file search baar

--- Project_Demos/src/App.jsx
+++ Project_Demos/src/App.jsx
@@ -1,6 +1,7 @@
 // import Counter from './components/Counter'
 import Calculator from './components/Calculator'
-import ToggleBackGroundColor from './components/ToggleBackGroundColor'
+import HiddenSearchBar from './components/HiddenSearchBar'
+// import ToggleBackGroundColor from './components/ToggleBackGroundColor'
 // import Meals from './components/Meals'
 // import Todo from './components/Todo'
 
@@ -13,7 +14,8 @@ function App() {
       {/* <Todo /> */}
       {/* <Meals/> */}
       {/* <Calculator/> */}
-      <ToggleBackGroundColor/>
+      {/* <ToggleBackGroundColor/> */}
+      <HiddenSearchBar/>
     </>
   )
 }
